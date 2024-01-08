73c6fcc2bf5ee968c67ac17e32ca93c156cc0029
Wildan <146868572+Wildan2023@users.noreply.github.com>
2024-01-09 00:19:58 +0700

    __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -1077,7 +1077,7 @@ def main():
         try:
             dispatcher.bot.send_photo(
                 f"@musicsupport_dan",
-                photo=f"https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+                photo=f"https://telegra.ph//file/04028a5c74d51feb92ba2.jpg",
                 caption=f"""
 ✨{BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ.
 ━━━━━━━━━━━━━

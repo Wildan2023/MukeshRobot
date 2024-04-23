ec999ba804550600e3352fbaadb6c0bbf28c386f
Wildan <146868572+Wildan2023@users.noreply.github.com>
2024-04-24 00:58:30 +0700

    must_join .py

--- must_join .py
+++ must_join .py
@@ -13,7 +13,7 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/Disney_storeDan" + MUST_JOIN
+                link = "" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
@@ -23,7 +23,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     f"Join Dulu Kampang Baru Pencet /start Lagi !!",
                 
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("Masuk sini nyet, Jangan Lupa Salam", url=f"https://t.me/Disney_storeDan)]
+                        [InlineKeyboardButton("Masuk sini nyet, Jangan Lupa Salam", url=f"https://t.me/Disney_storeDan")]
                     ])
                 )
                 await msg.stop_propagation()
